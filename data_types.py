9a93f502a15cf392f8367adb3a93fb66ad9446b8
Mike <Genrih3256@gmail.com>
2022-07-28 23:06:32 +0300

    We add new file

---
+++ data_types.py
@@ -0,0 +1,22 @@
+int_1 = 13
+int_2 = 14
+int_3 = 15
+int_4 = 16
+int_5 = 17
+variable_1 = "milk"
+variable_2 = "alkon"
+variable_3 = "heavy"
+variable_4 = "sun"
+variable_5 = "mars"
+print("1",type(variable_1))
+print("2",type(variable_2))
+print("3",type(variable_3))
+print("4",type(variable_4))
+print("5",type(variable_5))
+print("6",type(int_1))
+print("7",type(int_2))
+print("8",type(int_3))
+print("9",type(int_4))
+print("10",type(int_5))
+
+
