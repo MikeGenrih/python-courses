38c2b38384bd58f9288494033a7e25bcf63fd452
Mike <Genrih3256@gmail.com>
2022-07-28 22:32:16 +0300

    We add new file

---
+++ rows_and_numbers.py
@@ -0,0 +1,14 @@
+name = str(input("What is your name"))
+str_1 = str.strip(name)
+print("hello",str.capitalize(str_1))
+number = float(input("enter a number"))
+print(number)
+print(int(number))
+print(int(number)**4)
+print(int(number)**0.5)
+same_value = (number) % 2
+print("division remainder",(same_value))
+
+
+
+
