1b5c26be048e39713aae9ca1f5680e0da62bf9dc
Mike <Genrih3256@gmail.com>
2022-07-29 21:53:28 +0300

    We add new file

--- rows_and_numbers.py
+++ rows_and_numbers.py
@@ -1,7 +1,7 @@
 name = str(input("What is your name"))
 str_1 = str.strip(name)
 
-result = f"Hello {str_1}, nice to e-meet you! "
+result = f"Hello {str_1}, nice to e-meet you! "  #recorrected
 
 print(result) #
 number = float(input("enter a number"))
@@ -11,7 +11,7 @@ print(int(number))
 print(int(number)**4)
 print(int(number)**0.5)
 
-print("division remainder",(int(number) % 2))
+print("division remainder",(int(number) % 2))  # recorrected
 
 
 
