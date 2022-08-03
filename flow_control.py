153c808ab2e8d8a53f32ac162d5c5344511b56c9
Mike <Genrih3256@gmail.com>
2022-08-03 15:30:51 +0300

    We add new file

--- flow_control.py
+++ flow_control.py
@@ -1,21 +1,21 @@
 x = (input("num first"))
 #match x1 = "." in (x):
 if ("." in (x)):
-    x = float(x)
-else:
+    x = float(x)                                     # in this block the text from the user
+else:                                                # is converted to an integer or fractional number
     x = int(x)
 
 print(type(x))
 #################################
 sign = input("enter the sign")
-while not sign in ("+,-,/,*,**,%,"):
-    print(f"{sign=} is not correct.+,-,/,*,**,%,")
+while not sign in ("+,-,/,*,**,%,"):                 #in this block, I tried to loop the code
+    print(f"{sign=} is not correct.+,-,/,*,**,%,")   # until the user enters the correct sign
     sign = input("sign")
 #################################
 y = (input("num second"))
 
-if ("." in (y)):
-    y = float(y)
+if ("." in (y)):                                     #in this block the text from the user
+    y = float(y)                                     # is converted to an integer or fractional number
 else:
     y = int(y)
 
@@ -28,7 +28,7 @@ elif sign == "-":
     a = (x - y)
 elif sign == "/":
     if y == 0:
-        print("you can't devide by zero")
+        print("you can't devide by zero")            # here are the calculations of the calculator
     else:
        a = (x / y)
 
@@ -44,11 +44,12 @@ else:
 ###################################  4.0000
 b = int(a)
 if b == a:
-    a = int(a)
-
-else:
+    a = int(a)                                         #The calculator must return a result of type int if the operands
+                                                       # wereof type int. Or return a float if at least one operand was
+else:                                                  # a float
     a = float(a)
 print (f" {x}{sign}{y} = {a} answer is {(type(a))}")
+###################################
 
 
 
