8c4a6da4fd4c58a3a5aef9dea944f8a1984b5486
Mike <Genrih3256@gmail.com>
2022-08-16 13:23:42 +0300

    We add new file

--- Lists_and_tuples.py
+++ Lists_and_tuples.py
@@ -1,1 +1,17 @@
+# 1 . string from the user and prints every third word
 user = input('Enter any sring: ')
+                                                       #seafood crab massels prawns salmon squid tuna beef chicken duck
+print(f'every third word:{"!  ".join(user.split()[2::3])}')
+
+# 2  list comprehension
+initial_list = [1,2.1,"f","2",3,"1",18,"df",]
+
+print(f"{initial_list=}")
+                                       #[1,2.1,-1,"6",9,"3",18,-1]
+output_list = [el if type(el) == float
+               else el if type(el) == int and el%2 == 0 else el**2 if type(el) == int and el%2 != 0
+               else (str(int(float(el)*3)))  if type(el) == str and  el.isdigit() else -1 for el in initial_list]
+
+print(f"{output_list=}")
+
+print("    example=[1, 2.1, -1, '6', 9, '3', 18, -1]")
